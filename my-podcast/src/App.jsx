818aff4a8479446d31f9f460295a2cc4800ffa41
NelsonMALGAS <zmalgas69@gmail.com>
2023-08-04 11:37:53 +0200

    Modified App.jsx

--- my-podcast/src/App.jsx
+++ my-podcast/src/App.jsx
@@ -3,7 +3,6 @@ import "bootstrap/dist/css/bootstrap.css";
 import Header from "./Header";
 import MainContent from "./MainContents";
 import { getFromLocalStorage } from "./localStorage/locaLStorage";
-import { Helmet } from "react-helmet";
 import "./CSS/App.css";
 
 /**
@@ -34,43 +33,6 @@ function App() {
 
 	return (
 		<div className="app">
-			<Helmet>
-				<meta name="author" content="Nelson Zongezile Malgas" />
-				<link
-					rel="icon"
-					type="image/svg+xml"
-					href="https://the-audio-lounge.netlify.app/src/favicons/radio-outline.svg"
-				/>
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-				<title>The Audio Lounge</title>
-
-				<link
-					rel="apple-touch-icon"
-					sizes="180x180"
-					href="https://the-audio-lounge.netlify.app/src/meta/apple-touch-icon.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="32x32"
-					href="https://the-audio-lounge.netlify.app/src/meta/favicon-32x32.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="16x16"
-					href="https://the-audio-lounge.netlify.app/src/meta/favicon-16x16.png"
-				/>
-				<link rel="manifest" href="./src/meta/site.webmanifest" />
-				<link
-					rel="mask-icon"
-					href="https://the-audio-lounge.netlify.app/src/meta/safari-pinned-tab.svg"
-					color="#5bbad5"
-				/>
-				<meta name="msapplication-TileColor" content="#da532c" />
-				<meta name="theme-color" content="#ffffff" />
-			</Helmet>
-			;
 			<Header onSearch={handleSearch} />
 			<MainContent
 				searchTerm={searchTerm}
